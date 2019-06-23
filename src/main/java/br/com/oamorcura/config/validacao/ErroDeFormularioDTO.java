ac1902344f55edbb35ad0d4a064235111f66a52b
edson <edson12.j@hotmail.com>
2019-06-23 00:35:27 -0300

    refactor erro de formulario DTO

--- src/main/java/br/com/oamorcura/config/validacao/ErroDeFormularioDTO.java
+++ src/main/java/br/com/oamorcura/config/validacao/ErroDeFormularioDTO.java
@@ -1,28 +1,12 @@
 package br.com.oamorcura.config.validacao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErroDeFormularioDTO {
 	private String campo;
 	private String erro;
-	
-	public ErroDeFormularioDTO(String campo, String erro) {
-		this.campo = campo;
-		this.erro = erro;
-	}
-
-	public String getCampo() {
-		return campo;
-	}
-
-	public void setCampo(String campo) {
-		this.campo = campo;
-	}
-
-	public String getErro() {
-		return erro;
-	}
-
-	public void setErro(String erro) {
-		this.erro = erro;
-	}
 
 }
